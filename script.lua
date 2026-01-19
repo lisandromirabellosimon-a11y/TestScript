828467c0ac4cd0e91d33883adec975ef6385d4a6
lisandromirabellosimon-a11y <lisandromirabellosimon@gmail.com>
2026-01-19 19:49:47 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,25 @@
-print("---------------------------------")
-print("¡LOGRADO! El script desde GitHub funciona.")
-print("---------------------------------")
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "Menu de Lisandro",
+   LoadingTitle = "Cargando Interfaz...",
+   LoadingSubtitle = "GitHub Loader",
+})
+
+local Tab = Window:CreateTab("Principal", 4483362458)
+
+Tab:CreateButton({
+   Name = "Activar Noclip",
+   Callback = function()
+       local noclip = true
+       game:GetService("RunService").Stepped:Connect(function()
+           if noclip then
+               for _, v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+                   if v:IsA("BasePart") then v.CanCollide = false end
+               end
+           end
+       end)
+       Rayfield:Notify({Title = "Noclip", Content = "Activado correctamente"})
+   end,
+})
